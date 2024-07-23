6e75fafe4499db38510b7c8d4df5082594da0e77
PDM <ferrorist@naver.com>
2024-07-23 16:12:55 +0900

    Title: 반 대항전, Time: 104ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240723/반 대항전/class-confrontation.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.Arrays;
+public class Main {
+    static class myClass implements Comparable<myClass> {
+        char name;
+        int score;
+
+        public myClass(char name, int score){
+            this.name = name;
+            this.score = score;
+        }
+
+        @Override
+        public int compareTo(myClass o){
+            if(this.score == o.score) return this.name - o.name;
+            return o.score - this.score;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(in.readLine()); // 학생 n명
+
+        myClass[] arr = new myClass[4];
+        char WinnerClass = 'A';
+        
+        for(int i = 0; i < 4; i++){
+            String[] input = in.readLine().split(" ");
+
+            int sum = 0;
+            for(int j = 1; j < 6; j++)  sum += Integer.parseInt(input[j]);
+
+            arr[i] = new myClass((char)('A' + i), sum);
+            sb.append(arr[i].name).append(" - ").append(arr[i].score).append("\n");
+        }
+        Arrays.sort(arr);
+        sb.append("Class ").append(arr[0].name).append(" is winner!");
+
+        System.out.println(sb);
+    }
+}
